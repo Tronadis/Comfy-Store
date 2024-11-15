a206d1cb8496e5915901825053a9aea4a49c7f9d
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-15 13:11:16 +0100

    Add cart toggle and static mockup

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,10 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const toggleCartBtn = getElement('.toggle-cart');
+const cartOverlay = getElement('.cart-overlay');
+const cartClose = getElement('.cart-close');
+
+toggleCartBtn.addEventListener('click', () => cartOverlay.classList.add('show'));
+cartClose.addEventListener('click', () => cartOverlay.classList.remove('show'));
+
+export const openCart = () => cartOverlay.classList.add('show');
