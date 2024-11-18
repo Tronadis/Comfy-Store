c52ca30efa61689555cd69f0d73e4c5f8b9a7f33
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-18 15:16:40 +0100

    fetch data and restructure it

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
 import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
-const setupStore = () => {};
+const setupStore = (products) => {
+	store = products.map((product) => {
+		const {
+			id,
+			fields: { company, colors, featured, price, name, image: img },
+		} = product;
+		const image = img[0].thumbnails.large.url;
+		return { id, company, colors, featured, price, name, image };
+	});
+};
 const findProduct = () => {};
 export { store, setupStore, findProduct };
