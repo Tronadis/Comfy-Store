a05ef14b56c7e3945efa61303ec9f175fb07bac1
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-25 15:50:37 +0100

    fetch and display single product info

--- src/pages/product.js
+++ src/pages/product.js
@@ -7,9 +7,9 @@ import { addToCart } from '../cart/setupCart.js';
 import { singleProductUrl, getElement, formatPrice } from '../utils.js';
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
+const loading = getElement('.page-loading');
+const centerDOM = getElement('.single-product-center');
+const pageTitleDOM = getElement('.page-hero-title');
 // const imgDOM = getElement('.single-product-img');
 // const titleDOM = getElement('.single-product-title');
 // const companyDOM = getElement('.single-product-company');
@@ -19,6 +19,59 @@ import { singleProductUrl, getElement, formatPrice } from '../utils.js';
 // const cartBtn = getElement('.addToCartBtn');
 
 // cart product
-// let productID;
+let productID;
 
 // show product when page loads
+window.addEventListener('DOMContentLoaded', async () => {
+	const urlID = window.location.search; // `window.` isn't needed, it's implicit anyway.
+	try {
+		const response = await fetch(`${singleProductUrl}${urlID}`);
+		if (response.status >= 200 && response.status <= 299) {
+			const product = await response.json();
+			const { id } = product;
+			productID = id;
+			const { image, name, company, price, colors, description } = product.fields;
+			const { url: imageURL } = image[0].thumbnails.large;
+
+			document.title = `${name.toUpperCase()} | Comfy`;
+			pageTitleDOM.innerText = `Home / ${name}`;
+
+			const colorsSpan = colors.map((item) => {
+				return /*html*/ `<span class="product-color" style="background: ${item}"></span>`;
+			}).join(`
+                                `);
+			centerDOM.innerHTML = /*html*/ `
+                        <img src="${imageURL}" alt="${name}" class="single-product-img img" />
+                        <article class="single-product-info">
+                            <div>
+                                <h2 class="single-product-title">${name}</h2>
+                                <p class="single-product-company text-slanted">by ${company}</p>
+                                <p class="single-product-price">${formatPrice(price)}</p>
+                                <div class="single-product-colors">
+                                    ${colorsSpan}
+                                </div>
+                                <p class="single-product-desc">
+                                    ${description}.
+                                </p>
+                                <button class="addToCartBtn btn" data-id="${productID}">add to cart</button>
+                            </div>
+                        </article>     
+            `;
+			const cartBtn = getElement('.addToCartBtn');
+			cartBtn.addEventListener('click', function () {
+				addToCart(productID);
+			});
+		} else {
+			console.log(response.status, response.statusText);
+			centerDOM.innerHTML = /*html*/ `
+                <div>
+                    <h3>Sorry, something went wrong.</h3>
+                    <a href="index.html" class="btn">back home</a>
+                </div>`;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+
+	loading.style.display = 'none';
+});
