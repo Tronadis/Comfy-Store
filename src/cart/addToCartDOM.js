13c2dcabd75f1409ff93e71170c122297abc9d5e
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-27 18:46:48 +0100

    enable adding to cart
    I amended this commit because one changed file was unsaved the first time.

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -1,5 +1,25 @@
 import { formatPrice, getElement } from '../utils.js';
 
-const addToCartDOM = () => {};
+const cartItemsDOM = getElement('.cart-items');
+
+const addToCartDOM = ({ id, name, price, image, amount }) => {
+	const article = document.createElement('article');
+	article.classList.add('cart-item');
+	article.setAttribute('data-id', id);
+	article.innerHTML = /*html*/ `
+		<img src="${image}" alt="${name}" class="cart-item-img" />
+		<div>
+			<h4 class="cart-item-name">${name}</h4>
+			<p class="cart-item-price">${formatPrice(price)}</p>
+			<button class="cart-item-remove-btn" data-id="${id}">remove</button>
+		</div>
+		<div>
+			<button class="cart-item-increase" data-id="${id}"><i class="fas fa-chevron-up"></i></button>
+			<p class="cart-item-amount">${amount}</p>
+			<button class="cart-item-decrease" data-id="${id}"><i class="fas fa-chevron-down"></i></button>
+		</div>
+	`;
+	cartItemsDOM.appendChild(article);
+};
 
 export default addToCartDOM;
