7f3254212010276e8f6c71a449029c0fcba86cb4
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-29 16:21:16 +0100

    fix products page not fetching content by itself

--- src/pages/products.js
+++ src/pages/products.js
@@ -9,15 +9,26 @@ import setupCompanies from '../filters/companies.js';
 import setupPrice from '../filters/price.js';
 
 // specific imports
-import { store } from '../store.js';
+import fetchProducts from '../fetchProducts.js';
+import { setupStore, store } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
 
-const loading = getElement('.page-loading');
-const nameInput = getElement('.search-input');
-nameInput.value = ''; // fixes Firefox
-display(store, getElement('.products-container'));
-setupSearch(store);
-setupCompanies(store);
-setupPrice(store);
-loading.style.display = 'none';
+const init = async () => {
+	const loading = getElement('.page-loading');
+
+	if (store.length < 1) {
+		const products = await fetchProducts();
+		setupStore(products);
+	}
+
+	const nameInput = getElement('.search-input');
+	nameInput.value = ''; // fixes Firefox
+	display(store, getElement('.products-container'));
+	setupSearch(store);
+	setupCompanies(store);
+	setupPrice(store);
+	loading.style.display = 'none';
+};
+
+init();
